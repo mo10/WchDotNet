b61bf69ab4a542225e099bd3b7e87ad514ebca36
某10 <0w0@0w0.pw>
2022-09-05 02:25:03 +0800

    Add ChipDB. Add ReadChipInfo

--- wchisp/WCHUsbDevice.cs
+++ wchisp/WCHUsbDevice.cs
@@ -2,6 +2,7 @@
 using LibUsbDotNet.Main;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace wchisp
@@ -65,21 +66,51 @@ namespace wchisp
             UsbDevice = null;
         }
 
-        public bool IsVailed()
+        public Response Transfer(byte[] write_buf)
         {
             if (!this.Open())
                 throw new Exception("Failed to open device from registry");
 
-            var identify = Command.Identify(0, 0);
-            if (UsbWriter.Write(identify, 1000, out _) != ErrorCode.None)
+            if (UsbWriter.Write(write_buf, 1000, out _) != ErrorCode.None)
                 goto Failed;
-            byte[] buf = new byte[1024];
-            int bufLen;
-            if (UsbReader.Read(buf, 1000, out bufLen) != ErrorCode.None)
+
+            byte[] read_buf = new byte[1024];
+            int buf_len;
+            if (UsbReader.Read(read_buf, 1000, out buf_len) != ErrorCode.None)
                 goto Failed;
-            Console.WriteLine($"bufLen: {bufLen}");
+
+            return Response.FromRaw(read_buf, buf_len);
+
         Failed:
             throw new Exception(UsbDevice.LastErrorString);
+        }
+
+        public bool ReadChipInfo()
+        {
+            var buf = Command.Identify(0, 0);
+            var resp = Transfer(buf);
+
+            if (!resp.IsOK)
+            {
+                Console.WriteLine("Idenfity chip failed");
+                return false;
+            }
+
+            var chip_db = new ChipDB();
+
+            var chip = chip_db.FindChip(resp.Payload[0], resp.Payload[1]);
+            Console.WriteLine($"Found chip: {chip.Name}");
+
+            buf = Command.ReadConfig(Constants.CFG_MASK_ALL);
+            resp = Transfer(buf);
+            if (!resp.IsOK)
+            {
+                Console.WriteLine("read_config failed");
+                return false;
+            }
+            Console.WriteLine($"read_config: {string.Join(", ", resp.Payload.Select(b => b.ToString("X2")))}");
+
+            return true;
         }
     }
 }
